5d8240c596cda2f0169bb00be87b40a535a64aa1
David Solano <davidsolano0818@gmail.com>
2025-11-17 11:27:14 -0500

    Cambios 2.0

---
+++ client/.vite/deps/react-hook-form.js
@@ -0,0 +1,35 @@
+import {
+  Controller,
+  Form,
+  FormProvider,
+  Watch,
+  appendErrors,
+  createFormControl,
+  get,
+  set,
+  useController,
+  useFieldArray,
+  useForm,
+  useFormContext,
+  useFormState,
+  useWatch
+} from "./chunk-VSIQE7F2.js";
+import "./chunk-E55NSNTN.js";
+import "./chunk-4MBMRILA.js";
+export {
+  Controller,
+  Form,
+  FormProvider,
+  Watch,
+  appendErrors,
+  createFormControl,
+  get,
+  set,
+  useController,
+  useFieldArray,
+  useForm,
+  useFormContext,
+  useFormState,
+  useWatch
+};
+//# sourceMappingURL=react-hook-form.js.map
