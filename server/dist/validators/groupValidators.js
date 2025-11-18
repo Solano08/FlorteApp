247a5c62621a935e4bd33aec02ed12c687021ec7
David Solano <davidsolano0818@gmail.com>
2025-11-17 20:50:20 -0500

    Limpieza de Codigo

--- server/dist/validators/groupValidators.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createGroupSchema = void 0;
-const zod_1 = require("zod");
-exports.createGroupSchema = zod_1.z.object({
-    name: zod_1.z.string().min(3, 'El nombre debe tener al menos 3 caracteres'),
-    description: zod_1.z.string().max(500).optional(),
-    coverImage: zod_1.z.string().url().optional()
-});
